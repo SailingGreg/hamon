a728760395cd8e528c8e3326499848531e59bb12
Greg Brougham <greg.brougham@gmail.com>
2021-03-26 09:38:15 +0000

    InfluxDB example updates

--- indata.js
+++ indata.js
@@ -1,10 +1,14 @@
 /*
  * test program to write to influxDB
  *
+ * this just defines a simple data series with a couple of tags
  */
 
-
 const Influx = require('influx');
+
+// add nano date support for influxdb
+//import { toNanoDate } from 'influx'
+
 
 // table knx with source, groupaddress and value
 const influx = new Influx.InfluxDB({
@@ -14,13 +18,32 @@ const influx = new Influx.InfluxDB({
   password: 'Grafana',
   schema: [
     {
+      // the database 'table'
       measurement: 'knx',
       // we have INTEGER, FLOAT, STRING & BOOLEAN,
-      fields: { groupaddr: Influx.FieldType.STRING,
-		source: Influx.FieldType.STRING },
-		value: Influx.FieldType.FLOAT },
-      tags: ['unit', 'location']
+      fields: { value: Influx.FieldType.FLOAT },
+      tags: ['groupaddr', 'source']
     }
   ]
-})
+});
 
+const date = new Date();
+
+influx.writePoints([
+      {
+        measurement: 'knx',
+        tags: {
+          groupaddr: "0/1/0",
+          source: "1.1.14",
+        },
+        fields: { value: 20.12 },
+        timestamp: date,
+        //timestamp: date.toISOString(),
+      }
+    ], {
+      database: 'hamon',
+      precision: 'ms',
+    })
+    .catch(error => {
+      console.error(`Error saving data to InfluxDB! ${error.stack}`)
+    });
