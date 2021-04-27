c175f847e8c675b2cdf414dda42986a24766bca0
toheriman <e2h3in@gmail.com>
2021-04-27 13:49:09 +0200

    Worker threads

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ This is small project that use the knx.js stack to access and display
 KNX data via KNXnet/IP Router - in this case ha-test.dyndns.org
 
 Data is written to influxDB and then Grafana is used for display
+
+# To run
+node worker.js
--- ha-test.js
+++ ha-test.js
@@ -1,82 +1,48 @@
 /*
  Simple script to test knx.js and validate connectivity
  It will list all events exposed on the bus via the KNXnet/IP router
-
 */
+const { workerData } = require('worker_threads')
 
 // load knx ip stack
-var knx = require('knx');
-//const dns = require('dns');
-var dnsSync = require('dns-sync');
-const yaml = require('js-yaml');
-const fs   = require('fs');
+var knx = require('knx')
+var dnsSync = require('dns-sync')
 
-var knxnetIP = "ha-test.dyndns.org";
-var knxnetAddr = "";
-var knxnetPort = 50001;
+const { knxnetIP, knxnetPort, name } = workerData
 
-fileArg = "hamon.yml";
-
-//console.log(process.argv);
-var knxLoc = process.argv[2];
-if (knxLoc == undefined || knxLoc == "") {
-    console.log ("usage: %s {location}", process.argv[1]);
-    return 1;
-}
-
-if (fs.existsSync("./" + fileArg)) {
-    console.log ("Parsing configuration file %s", fileArg);
-} else {
-    console.log ("Configuration file %s doesn't exist", fileArg);
-    return 1;
-}
-
-// this parse and expands the configuration
-const doc = yaml.load(fs.readFileSync(fileArg, 'utf8'));
-cnt = 0;
-for (deploy in doc["locations"]) {
-    cnt = cnt + 1;
-    install = doc["locations"][deploy]
-    console.log("\t %s %s %s %d", deploy, install['name'], install['dns'], install['port']);
-    if (install['name'] == knxLoc) {
-        knxnetIP = install['dns'];
-        knxnetPort = install['port'];
-        knxnetXML = install['config'];
-    }
-}
 
 // resolve the KNXnet/IP router
-console.log("Running for %s ", knxLoc);
-
-// resolve the KNXnet/IP router
-knxnetAddr = dnsSync.resolve(knxnetIP);
-console.log('KNXnet/IP %s -> %s:%d', knxnetIP, knxnetAddr, knxnetPort);
+knxnetAddr = dnsSync.resolve(knxnetIP)
 
 // and create connection
 var connection = knx.Connection({
-
- // the following is the ip address for ha-test.dyndns.org
- // as the module needs ipv4/ipv6 address
- ipAddr: knxnetAddr, ipPort: knxnetPort,
- // may be incorrect
- //physAddr: '0.1.0',
- // ensure it tunneling and not operating n hybrid mode
- forceTunneling: true,
- // the defaut is info
- //loglevel: 'debug',
- handlers: {
-  connected: function() {
-    console.log('Connected!');
-  },
-
-  event: function (evt, src, dest, value) {
-   console.log("%s **** KNX EVENT: %j, src: %j, dest: %j, value: %j",
-    new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-    evt, src, dest, value);
-  },
-
-  error: function(connstatus) {
-      console.log("**** ERROR: %j", connstatus);
+  // the following is the ip address for ha-test.dyndns.org
+  // as the module needs ipv4/ipv6 address
+  ipAddr: knxnetAddr,
+  ipPort: knxnetPort,
+  // may be incorrect
+  // physAddr: '0.1.0',
+  // ensure it tunneling and not operating n hybrid mode
+  forceTunneling: true,
+  // the default is info
+  // logLevel: 'debug',
+  handlers: {
+    connected: function () {
+      console.log('Connected!')
+    },
+    event: function (evt, src, dest, value) {
+      console.log(
+        '%s **** KNX EVENT: %j, src: %j, dest: %j, value: %j, name: %j',
+        new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+        evt,
+        src,
+        dest,
+        value, 
+        name
+      )
+    },
+    error: function (connstatus) {
+      console.log('**** ERROR: %j', connstatus)
+    }
   }
- }
-});
+})
