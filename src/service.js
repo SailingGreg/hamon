2daf3cff0f290c268537fe7f227bfe025be9c6ea
toheriman <e2h3in@gmail.com>
2021-04-29 17:04:11 +0200

    Detect changes and store threads

--- src/service.js
+++ src/service.js
@@ -2,37 +2,53 @@ const fs = require('fs')
 const { Worker } = require('worker_threads')
 const yaml = require('js-yaml')
 const logger = require('./logger')
-
 const dnsSync = require('dns-sync')
-var dnsEntry = "";
-var lastworker = "";
 
-async function runService(hamonConfig) {
-  const doc = yaml.load(fs.readFileSync(hamonConfig, 'utf8'))
+let firstRun = true
+var dnsEntry = ''
+const threads = new Set()
+let workerLocations = new Map()
+
+const getDoc = (hamonConfig) => yaml.load(fs.readFileSync(hamonConfig, 'utf8'))
+
+async function ConnectionService(doc) {
   for (location in doc.locations) {
     const loc = doc.locations[location]
     logger.info(`${loc.name} ${loc.dns} ${loc.port}`)
-	  dnsEntry = loc.dns;
-    new Promise((resolve, reject) => {
-      const worker = new Worker('./src/connection.js', {
-        workerData: { location: loc }
-      })
-	    if (loc.name == "office") {
-	        lastworker = worker;
-	    }
-      worker.on('message', resolve)
-      worker.on('error', reject)
-      worker.on('exit', (code) => {
-        if (code !== 0)
-          reject(new Error(`Worker stopped with exit code ${code}`))
-      })
+    dnsEntry = loc.dns
+    const worker = new Worker('./src/connection.js', {
+      workerData: { location: loc }
+    })
+    workerLocations.set(loc.name, worker)
+    threads.add(worker)
+  }
+  for (let worker of threads) {
+    worker.on('error', (err) => {
+      throw err
+    })
+    worker.on('exit', () => {
+      threads.delete(worker)
+      console.log(`Thread exiting, ${threads.size} running...`)
     })
   }
-    // do a syncDNS and then terminate
-    const knxAddr = dnsSync.resolve(dnsEntry)
-    console.log("dnsEntry %s", dnsEntry);
-    lastworker.postMessage({ exit: true });
-    console.log("terminated");
+  // do a syncDNS and then terminate
+  const knxAddr = dnsSync.resolve(dnsEntry)
+  console.log('knxAddr', knxAddr)
+  const workersIterator = threads.values()
+  const worker = workersIterator.next().value
+  console.log('dnsEntry %s', dnsEntry)
+  worker.postMessage({ exit: true })
+}
+
+async function runService(hamonConfig) {
+  fs.watch(hamonConfig, function (event, filename) {
+    return ConnectionService(getDoc(hamonConfig))
+  })
+
+  if (firstRun) {
+    firstRun = false
+    return ConnectionService(getDoc(hamonConfig))
+  }
 }
 
 module.exports.runService = runService
