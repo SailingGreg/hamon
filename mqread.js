7c2f79a18c734d4a85154c1b77a28537dcd049a2
Toher <e2h3in@gmail.com>
2021-05-27 16:11:48 +0200

    Mq write

--- mqread.js
+++ mqread.js
@@ -5,18 +5,21 @@
  */
 
 const mqtt = require('mqtt'); // added to package.json
-
+const ets = require('./parsexml')
 const MQTTBROKERIP = "mqtt://localhost"; // needs protocol which is mqtt
 const MQTTBROKERPORT = 1883; // default
 const MQTTUSERNAME = "";
 const MQTTPASSWORD = "";
-
+const { writeEvents } = require('./src/db')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const logger = require('./src/logger')
 const topicPrefix = ""; // blank for now
 // need to add 'location' to following for the thread
-const gadRegExp = new RegExp((topicPrefix || 'knx/') + '(\\d+)\/(\\d+)\/(\\d+)\/(\\w+)(\/([\\w\\d]+))?');
-
+const gadRegExp = new RegExp((topicPrefix || 'knx/') + '(\\w+)/(\\d+)\/(\\d+)\/(\\d+)\/(\\w+)');
 let mqttClient  = mqtt.connect(MQTTBROKERIP);
 
+const getDoc = (hamonConfig) => yaml.load(fs.readFileSync(hamonConfig, 'utf8'));
 /*
  * We can use a "knx/{location}/+/+/+/+" format 
  */
@@ -35,13 +38,57 @@ mqttClient.on('message', function (topic, message) {
 
     // need to parse the topic - does it have read/write?
     if (topic.startsWith("knx")) { // guard
-
-        // extract group address
-        let gadArray = gadRegExp.exec(topic);
-        let gad = gadArray[1] + "/" + gadArray[2] + "/" + gadArray[3];
-        let cmd = gadArray[4];
-        console.log("> address/command: %s %s", gad, gadArray[4]);
-
+        console.log("topic", topic)
+      //  extract group address
+       let gadArray = gadRegExp.exec(topic);
+  
+       if(!gadArray) return
+        let location = gadArray[1]
+        let dest = gadArray[2] + "/" + gadArray[3] + "/" + gadArray[4];
+        let cmd = gadArray[5];
+        const rest = (gadArray.input.replace(gadArray[0]+"/", "")).split('/')
+        console.log("> address/command: %s %s", location, dest, cmd);
+        const path = "/home/e2h3/Dev/hamon/"
+        const hamonConfig = getDoc(path + 'hamon.yml')
+        let config;
+        for ( let locale in hamonConfig.locations ) {
+            if (hamonConfig.locations[locale].name === location) {
+                config = hamonConfig.locations[locale].config
+            }
+        }
+        const groupAddresses = ets.parsexml(path + config) || {}
+        if (groupAddresses.hasOwnProperty(dest)) {
+            ctime = localDate().replace(/T/, ' ').replace(/\..+/, '')
+            const evt = "GroupValue_Write"
+            console.log('ctime', ctime)
+            console.log('dest', groupAddresses[dest])
+            const src = rest[0]
+            logger.info(
+              '>> %s Event %s %j -> %j (%s - %s) - %j %s %j',
+              ctime,
+              evt,
+              src,
+              dest,
+              groupAddresses[dest].name,
+              rest[1],
+              msg,
+              "unit",
+              typeof(msg)
+            )
+            // encode the evt to shorten it - "gw" or "re"
+            // if (groupAddresses[dest].type != undefined 
+            //         && groupAddresses[dest].type != '') { // if type
+                writeEvents(
+                    evt,
+                    src,
+                    dest,
+                    location,
+                    groupAddresses[dest].name,
+                    rest[1],
+                    msg,
+                )
+            // }
+          }
         // is the group address valid & a write??
         /*
         if (groupAddresses.hasOwnProperty(gad)) {
@@ -52,7 +99,7 @@ mqttClient.on('message', function (topic, message) {
                     groupAddresses[gad].endpoint.write(msg);
                 } catch (err) {
                     logger.error('Could not write message %j to group address %s, err: %s', parsedMessage, gad, err);
-            } else f (cmd == 'read') { // read
+            }} else if (cmd == 'read') { // read
                     groupAddresses[gad].endpoint.read();
             } else if (cmd == 'on') {
                 //check if on is okay and then action
@@ -66,8 +113,18 @@ mqttClient.on('message', function (topic, message) {
                 logger.info("Invalid command %s", cmd);
             }
         }
-        */
+    }
+       */ 
     }
 
-    // client.end()
+   // client.end()
 })
+
+
+function localDate() {
+    var date = new Date()
+    var isoDateTime = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60000
+    ).toISOString()
+    return isoDateTime
+  }
