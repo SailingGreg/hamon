185fcde4d1aad5e8ca0161a2cf927d0085b8245c
Greg Brougham <greg.brougham@gmail.com>
2022-03-18 09:41:06 +0000

    Added container check

--- restart.sh
+++ restart.sh
@@ -8,15 +8,17 @@ log=$loc/tmp/restart.log
 
 # note the json passed on stdin
 # should be $$ for process file so unique
+# comment when debugging
 cat > $loc/tmp/alert.json
 
 # note run and user
 date >> $log
 whoami >> $log
 
-# extract the location
-location=`sed -n 's|^.*"tags":{"location":"\([a-z]*\)"}.*$|\1|p' < $loc/tmp/alert.json`
+# extract the location from the args
+location=`sed -n 's|^.*"tags":{"location":"\([a-z1-9]*\)"}.*$|\1|p' < $loc/tmp/alert.json`
 
+# do we have a location
 if [ "$location" == "" ]; then
 	echo "$0 {site}"
 	exit 1
@@ -24,19 +26,39 @@ fi
 
 site=$location
 
+# and is the location defined
+cnt=`grep -c -m 1 $site $loc/hamon.yml`
+if [ $cnt -eq 0 ]; then
+	echo "Site $site not defined" >> $log
+	echo "site $site not defined"
+	exit 1
+fi
+
+# find the xml file and address from hamon.yml
+config=`grep -A 8 " name: $site" $loc/hamon.yml | grep config | awk  '{print $2}'`
+addr=`grep -A 8 " name: $site" $loc/hamon.yml | grep dns | awk  '{print $2}'`
+
+# is this a container that is "172."
+if [[ $addr == 172.* ]]; then
+	cont=true
+
+	# need to restart the right container so we need a name mapping
+	# and we based this on the container be the name of the location
+
+	#docker restart $site
+	#exit 0
+fi
+
 #echo "Restarting $site"
-echo "Restarting $site" >> $log
+echo "Restarting $site $config $addr $cont" >> $log
+
+# ADDED FOR DEBUG
+#exit 0
 
 # write it to the named pipe so hamon can action
 echo $site > $loc/tmp/kpipe
 
-cnt=`grep -c -m 1 $site $loc/hamon.yml`
 
-#echo $cnt
-if [ $cnt -eq 0 ]; then
-	echo "site $site not defined"
-	exit 1
-fi
 
 # could grep the hamon.yml file and then fail back
 #xml=`grep "$site.*xml" $loc/hamon.yml | awk '{print $2}'`
