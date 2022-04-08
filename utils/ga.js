00da204092df64db9988f542dbbbae1a2b7b4c16
Tom Kulpa <tom.kulpa.work@gmail.com>
2022-04-08 17:25:50 +0200

    Add parseInt to formatGroupAddress

--- utils/ga.js
+++ utils/ga.js
@@ -1,5 +1,6 @@
 function formatGroupAddress(value) {
-  return `${(value & 0x7800) >> 11}/${(value & 0x700) >> 8}/${value & 0xff}`
+  const num = parseInt(value)
+  return `${(num & 0x7800) >> 11}/${(num & 0x700) >> 8}/${num & 0xff}`
 }
 
 exports.formatGroupAddress = formatGroupAddress
