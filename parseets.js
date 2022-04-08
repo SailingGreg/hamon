0a80583fdbc147a4db88733569595c7e14957db3
Tom Kulpa <tom.kulpa.work@gmail.com>
2022-04-08 12:51:00 +0200

    Add ETS Project Parser

--- parseets.js
+++ parseets.js
@@ -5,40 +5,41 @@
  *
  */
 
-const ets = require('./parsexml');
 const fs = require('fs');
+const { etsProjectParser } = require('./utils/etsProjectParser');
 
 //console.log(process.argv);
 var fileArg = process.argv[2];
 if (fileArg == undefined || fileArg == "") {
     console.log ("usage: %s {filename}", process.argv[1]);
-    return 1;
+    process.exit(1)
 }
 
 if (fs.existsSync("./" + fileArg)) {
     console.log ("Parsing file %s", fileArg);
 } else {
     console.log ("File %s doesn't exist", fileArg);
-    return 1;
+    process.exit(1)
 }
 
-// parse the etx xml exmport
-let groupAddresses = ets.parsexml("./" + fileArg) || {};
-
-addr = 0;
-dpts = 0;
-ga = 0;
-for (let key in groupAddresses) {
-    addr++;
-    if (groupAddresses.hasOwnProperty(key)) {
-        if (groupAddresses[key].dpt != undefined)
-            dpts++;
-        console.log(key, groupAddresses[key].dpt,
-            groupAddresses[key].name);
-        ga++;
+etsProjectParser("./" + fileArg).then((project) => {
+    console.log('project', project)
+    addr = 0;
+    dpts = 0;
+    ga = 0;
+    const groupAddresses = project.groupAddresses
+    for (let key in groupAddresses) {
+        addr++;
+        if (groupAddresses.hasOwnProperty(key)) {
+            if (groupAddresses[key].dpt != undefined)
+                dpts++;
+            console.log(key, groupAddresses[key].dpt,
+                groupAddresses[key].name);
+            ga++;
+        }
     }
-}
-console.log("%s has %d entries, %d with values and %d DPTs",
- 					 fileArg, addr, ga, dpts);
+    console.log("%s has %d entries, %d with values and %d DPTs",
+                          fileArg, addr, ga, dpts);
+})
 
 // end of file
